51f10afd466bcfe2a49ed76707c09aa54d51a1b2
Siddharth Mukkanawar <63512310+SiddharthM29@users.noreply.github.com>
2021-12-20 18:51:02 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="indexcss.css">
+</head>
+<body>
+    <header class="heading">
+        <img src="TSF_Logo.jpg" height="110px" width="150px">
+      <center> <div> <h1>Welcome To TSF Bank</h1></div> </center>
+    </header>
+    <nav class="navbar">
+        <a href="inex.php">Home</a>
+        <a href="https://www.thesparksfoundationsingapore.org/">About </a>
+        <a href="https://internship.thesparksfoundation.info/">Careers</a>
+    </nav>
+    <section  class="section">
+        <img class="image" src="5_Steps_to_Digital_Transformationin_Banking_FRS.jpg" alt="banking">
+        <button class="btn"><a href="viewcustomer.php">View customers</a> </button> <br> <br>
+        <button class="btn"><a href="Transactions.php"> Transaction</a> </button> <br> <br>
+        <button class="btn"><a href="Transacthistory.php">Transaction History</a></button>
+    </section>
+    <br> 
+    <footer class="foot">
+        <h1 style="color: blue;">Contact us</h1>
+        <br>
+        <p>EmailId - bank@bank.ac.in</p><br>
+        <p>mobile 9898982878</p>
+    </footer>
+</body>
+</html>
