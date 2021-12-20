48c07c501d239c4f9afa302455a5631960c3d25d
Siddharth Mukkanawar <63512310+SiddharthM29@users.noreply.github.com>
2021-12-20 18:57:47 +0530

    Add files via upload

--- PHP/connection.php
+++ PHP/connection.php
@@ -1,1 +1,11 @@
-
+<?php
+$server="localhost";
+$username="root";
+$password="sidhu@2904";
+$db="tsf_bank";
+$con=mysqli_connect($server,$username,$password,$db);
+if(!$con)
+{
+    die("connection unsuccessfull due to ".mysqli_conncect_error());
+}
+?>
